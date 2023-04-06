e9eac3b0ace1f4a2ef206229ec11119f45a12180
Lars2203B <122023822+Lars2203B@users.noreply.github.com>
2023-04-06 14:56:53 +0200

    Add files via upload

---
+++ Pepper_quiz_31032023/translations/translation_en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, I am Pepper. Today we are going to practice multiplication tables.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, I am Pepper. Today we are going to practice multiplication tables.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you ready to start?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you ready to start?</translation>
+        </message>
+    </context>
+</TS>
